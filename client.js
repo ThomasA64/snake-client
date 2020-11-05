8eba05c9b16cd75c5be1252b4d72bb575cc5933c
Thomas Arthur <67028802+ThomasA64@users.noreply.github.com>
2020-11-05 09:48:52 -0500

    Update client.js

--- client.js
+++ client.js
@@ -9,13 +9,6 @@ const connect = function() {
   return conn;
 }
 
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incoming data as text
-  
   conn.setEncoding('utf8'); 
 
   return conn.on('Successfully connected to the game server');

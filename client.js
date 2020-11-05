97c23606aafc56416c57adf17edb145282352285
Thomas Arthur <67028802+ThomasA64@users.noreply.github.com>
2020-11-05 09:48:12 -0500

    Updated to send name

--- client.js
+++ client.js
@@ -9,3 +9,17 @@ const connect = function() {
   return conn;
 }
 
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: 'localhost',
+    port: 50541
+  });
+  // interpret incoming data as text
+  
+  conn.setEncoding('utf8'); 
+
+  return conn.on('Successfully connected to the game server');
+}
+conn.on('data', (data) => {
+  console.log('Server says: Name: TGA', data);
+});

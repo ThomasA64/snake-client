33e1336b547fc8ccb7b01b80987855686d1b490a
Thomas Arthur <67028802+ThomasA64@users.noreply.github.com>
2020-12-02 13:48:24 -0500

    Completed the Client Setup and Connection Module

--- client.js
+++ client.js
@@ -1,18 +1,26 @@
+const net = require('net');
+/**
+ * Establishes connection with the game server
+ */
 const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
+  const conn = net.createConnection({
+    host: "localhost",
     port: 50541
   });
   // interpret incoming data as text
-  conn.setEncoding('utf8'); 
+  conn.setEncoding('utf8');
 
+  conn.on('connect', () => {
+
+    console.log('Successfully connected to the Server')
+  })
+  conn.on('data', (message) => {
+
+    console.log(message)
+  })
+  
   return conn;
 }
 
-  conn.setEncoding('utf8'); 
-
-  return conn.on('Successfully connected to the game server');
-}
-conn.on('data', (data) => {
-  console.log('Server says: Name: TGA', data);
-});
+module.exports = {connect} 
+//* Using curly braces and making it an object allows for adding more items for export. 
